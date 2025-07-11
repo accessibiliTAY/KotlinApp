8a206a39df3bba68ffb6ef9652284ad823cf5855
Tay Ashton <taylor.ashton17@gmail.com>
2025-07-11 03:04:07 +0000

    Chapter 4 challenge. when loop

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -46,68 +46,25 @@ fun GreetingPreview() {
     }
 }
 
-fun getMessage(input: Int) = if (input > 3) {
-    "Greater than 3"
-} else {
-    "Not greater than 3"
-}
 
-fun getMessageWithWhen(input: Int) = when (input) {
-    3 -> "Value is 3"
-    else -> "Value is not 3"
-}
-
-fun checkType(input: Any?) {
-    when (input) {
-        null -> println("Input was null")
-        is String -> println("Input was a String of length ${input.length}")
+// Refactor getOutput() to use a when expression
+fun getOutput(input: Any?): String = when(input) {
+    is Number -> {
+        if (input !is Int) {
+            "Input was an Int"
+        } else {
+            "Input was a non-Int Number"
+        }
     }
+    is String -> {"Input was a String with length ${input.length}"}
+    null -> {"Input was null" }
+    else -> {"Input didn't match target inputs"}
 }
 
 fun main() {
-    var someVariable = 100
-
-//    when {
-//        someVariable > 3 -> println("The value was greater than 3")
-//        someVariable > 2 -> println("The value was greater than 2")
-//        else -> {
-//            println("Not greater")
-//        }
-//    }
-
-    when (someVariable) {
-        0,1 -> println("The value was 0 or 1")
-        2 -> println("The value is 2")
-        3 -> println("The value is 3")
-        in 4..Int.MAX_VALUE -> println("The value was greater than 3")
-    }
-    var anotherVariable = 0
-
-    val message = when (anotherVariable) {
-        3 -> "The value is 3"
-        else -> "The value is not 3"
-    }
-
-    val anothermessage = try {
-        throw IllegalStateException()
-        "The value is ${10 / 0}"
-    } catch (error: ArithmeticException) {
-        "Error was thrown"
-    } catch (error: java.lang.IllegalStateException) {
-        "Error was IllegalState"
-    }
-    println(anothermessage)
-
-    val aGenericVariable: Any = 5
-
-    checkType("null")
-
-    var counter = 0
-
-    do {
-        println(counter)
-        counter += 1
-    } while (counter < 5)
-
-    for (i in 10 downTo 0 step 3) println(i)
+    println(getOutput(null))
+    println(getOutput(4))
+    println(getOutput(3.2))
+    println(getOutput("Hello Kotlin"))
+    println(getOutput('a'))
 }
